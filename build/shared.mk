4b728e1c796fdcef5b177ea025bf3a115bc1b781
Scofield626 <765203006@qq.com>
2024-04-24 15:33:34 +0000

    new build

--- build/shared.mk
+++ build/shared.mk
@@ -83,7 +83,7 @@ FLAGS += -DDIRECTPATH
 endif
 
 CFLAGS = -std=gnu11 $(FLAGS)
-CXXFLAGS = -std=gnu++20 $(FLAGS)
+CXXFLAGS = -std=gnu++17 $(FLAGS)
 
 # handy for debugging
 print-%  : ; @echo $* = $($*)
